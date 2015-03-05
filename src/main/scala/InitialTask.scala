ade541d77d88983b25543fef4d7070a742efccc7
IngrainTea <jsfdiffp@gmail.com>
2015-03-05 14:03:53 +0300

    Update task1

--- src/main/scala/InitialTask.scala
+++ src/main/scala/InitialTask.scala
@@ -3,7 +3,37 @@
  */
 
 object InitialTask extends App {
-  
+
+  /**
+   * This method computes the sum of all elements in the list xs.
+   *
+   * For this example assignment you can use the following methods in class
+   * `List`:
+   *
+   *  - `xs.isEmpty: Boolean` returns `true` if the list `xs` is empty
+   *  - `xs.head: Int` returns the head element of the list `xs`. If the list
+   *    is empty an exception is thrown
+   *  - `xs.tail: List[Int]` returns the tail of the list `xs`, i.e. the the
+   *    list `xs` without its `head` element
+   *
+   *  ''Hint:'' instead of writing a `for` or `while` loop, think of a recursive
+   *  solution.
+   *
+   * @param xs A list of natural numbers
+   * @return The sum of all elements in `xs`
+   */
+  def sum(xs: List[Int]): Int = {
+    var s = 0
+    xs.foreach(n => s += n)
+    s
+  }
+
+
+
+
+
+
+
 }
 
 
