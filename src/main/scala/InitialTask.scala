ea9fb9459e766903a7b3e049d6044c737cf220e2
IngrainTea <jsfdiffp@gmail.com>
2015-03-05 16:24:20 +0300

    Changes in task 1

--- src/main/scala/InitialTask.scala
+++ src/main/scala/InitialTask.scala
@@ -23,10 +23,45 @@ object InitialTask extends App {
    * @return The sum of all elements in `xs`
    */
   def sum(xs: List[Int]): Int = {
+
+
     var s = 0
     xs.foreach(n => s += n)
     s
   }
+
+  /**
+   * This method returns the largest element in a list of integers. If the
+   * list `xs` is empty it throws a `java.util.NoSuchElementException`.
+   *
+   * You can use the same methods of the class `List` as mentioned above.
+   *
+   * ''Hint:'' Again, think of a recursive solution instead of using looping
+   * constructs. You might need to define an auxiliary method.
+   *
+   * @param xs A list of natural numbers
+   * @return The largest element in `xs`
+   * @throws java.util.NoSuchElementException if `xs` is an empty list
+   */
+  def max(xs: List[Int]): Int = {
+    var x = 0
+    val msg = "No elements in List"
+    try {
+      xs.foreach(n => if(n > x) x = n else n)
+      x
+    } catch {
+      case ex: java.util.NoSuchElementException => {
+        println("No elements in List")
+      }
+        -1
+    }
+  }
+
+
+
+  val ds = List()
+
+  println(sum(ds))
 
 
 
